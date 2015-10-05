c9ca8e76f1e7b5b9295d3ef7c64c8a23b632073e
Nick Diedrich <nick.diedrich@haw-hamburg.de>
2015-10-05 20:33:38 +0200

    added language preference

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ var self = require('sdk/self');
 var contextMenu = require("sdk/context-menu");
 var tabs = require("sdk/tabs");
 var selection = require("sdk/selection");
+var simplePrefs = require('sdk/simple-prefs');
 
 var contextMenu = contextMenu.Item({
         label: "Wikipedia Search",
         context: contextMenu.SelectionContext(),
         contentScript: 'self.on("click", function () { self.postMessage(); });',
         onMessage: function () {
-           tabs.open("https://en.wikipedia.org/w/index.php?search="+selection.text+"&title=Special%3ASearch&fulltext=Search");
+           tabs.open("https://"+simplePrefs.prefs['language']+".wikipedia.org/w/index.php?search="+selection.text+"&title=Special%3ASearch&fulltext=Search");
         }
     });
